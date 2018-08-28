9e782981652262ca21b8f7223e2bfcf6b85c3589
Umut Can Akçay <32799287+Squzy@users.noreply.github.com>
2018-08-29 01:56:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,8 +17,9 @@ var eightball = [ // sets the answers to an eightball
 var bot = new Discord.Client(); // sets Discord.Client to bot
 
 bot.on("ready", function() { // when the bot starts up, set its game to Use *help and tell the console "Booted up!"
-    bot.user.setGame(">yardım") // sets the game the bot is playing
-    console.log("Booted up!") // messages the console Booted up!
+      bot.user.setGame('>yardım')
+      bot.user.setStatus('Hyosukenin biricik sevgilisi')
+    console.log("Uçmaya hazır mıyız!") // messages the console Booted up!
 });
 
 bot.on("message", function(message) { // when a message is sent
