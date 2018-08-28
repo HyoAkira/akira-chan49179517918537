df2f1da700c075df37790210eb1107d9da63c80d
Umut Can Akçay <32799287+Squzy@users.noreply.github.com>
2018-08-29 00:44:15 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,10 +35,7 @@ client.on('message', msg => {
 });
 
 // Söyle
- if (message.startsWith("say") == true) {//Check if the message send starts with "say"
-    var newMessage = message.replace("say ", "");//Making a variable where "say " is removed
-    bot.sendMessage({to: channelID, message:newMessage})//Send the new variable.
-}
+
 
 // Bu olmak zorunda
 client.login(process.env.BOT_TOKEN);
