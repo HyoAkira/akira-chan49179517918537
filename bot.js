ba6b0c4594310d9dbf2d210837f3e5727fe0a735
Umut Can Akçay <32799287+Squzy@users.noreply.github.com>
2018-08-29 00:32:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,12 +5,34 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
+
+// Akira Yenilmez
 client.on('message', message => {
     if (message.content === 'akira') {
     	message.reply('beni mi deniyorsun? Ben yenilmezim.');
   	}
 });
 
+
+// pingim kaç
+
+client.on('message', msg => {
+  if (msg.content.toLowerCase() === prefix + 'ping') {
+    if (msg.channel.type !== "dm") {
+      const ozelmesajkontrol = new Discord.RichEmbed()
+    .setColor(0x00AE86)
+    .setTimestamp()
+    .setAuthor(msg.author.username, msg.author.avatarURL)
+    .addField(':ping_pong: Pong!', 'Özel mesajlarını kontrol et. :postbox:');
+    msg.channel.sendEmbed(ozelmesajkontrol) }
+      const pingozel = new Discord.RichEmbed()
+    .setColor(0x00AE86)
+    .setTimestamp()
+    .setAuthor(msg.author.username, msg.author.avatarURL)
+    .addField('Ping:', '**' + client.ping + '**') 
+    return msg.author.sendEmbed(pingozel);
+  }
+});
 
 // Bu olmak zorunda
 client.login(process.env.BOT_TOKEN);
