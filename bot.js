07e2a7a847c3f63022166d5f14e3a8b7e63ed85d
Umut Can Akçay <32799287+Squzy@users.noreply.github.com>
2018-08-29 00:38:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
+const prefix = ">";
 client.on('ready', () => {
     console.log('I am ready!');
 });
@@ -33,6 +33,17 @@ client.on('message', msg => {
     return msg.author.sendEmbed(pingozel);
   }
 });
+
+// Söyle
+ if(command === "söyle") {
+    // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
+    // To get the "message" itself we join the `args` back into a string with spaces: 
+    const sayMessage = args.join(" ");
+    // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
+    message.delete().catch(O_o=>{}); 
+    // And we get the bot to say the thing: 
+    message.channel.send(sayMessage);
+  }
 
 // Bu olmak zorunda
 client.login(process.env.BOT_TOKEN);
