6a2db8029f67976573810ced1076ed103fd13528
Umut Can Akçay <32799287+Squzy@users.noreply.github.com>
2018-08-31 01:45:55 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -142,5 +142,30 @@ function resetBot(channel) {
     .then(() => bot.login(process.env.BOT_TOKEN));
 }
 
+// mesaj silme
+exports.run = function(client, message, args) {
+
+      if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Hayır.");
+      if(!args[0]) return message.channel.send("hayır");
+      message.channel.bulkDelete(args[0]).then(() => {
+      message.channel.send(`${args[0]} mesaj temizlendi.`).then(msg => msg.delete(2000));
+    });
+
+    }
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ['clear','prune'],
+  permLevel: 2
+};
+
+exports.help = {
+  name: 'temizle',
+  description: 'Belirlenen miktar mesajı siler.',
+  usage: 'temizle <temizlenecek mesaj sayısı>'
+};
+
+
 // Bu olmak zorunda
 bot.login(process.env.BOT_TOKEN);
