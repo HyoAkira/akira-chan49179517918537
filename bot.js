3f976a6a91f5e201d5152a5a9a3fb8afb49109cc
Umut Can Akçay <32799287+Squzy@users.noreply.github.com>
2018-08-31 04:19:47 +0300

    temizle eklendi

--- bot.js
+++ bot.js
@@ -156,44 +156,59 @@ function resetBot(channel) {
 }
 
 // mesaj silme
-if (command == "havalandır") {
+client.on('message', msg => {
     if (msg.content.toLowerCase() === prefix + 'temizle') {
-        if (msg.channel.type === 'dm') {
-          const ozelmesajuyari = new Discord.RichEmbed()
-        .setColor(0xFF0000)
-        .setTimestamp()
-        .setAuthor(msg.author.username, msg.author.avatarURL)
-        .addField(':warning: Uyarı :warning:', 'Bu komutu özel mesajlarda kullanamazsın.')
-        msg.author.sendEmbed(ozelmesajuyari); }
-          if (msg.channel.type !== 'dm') {
-            if (!msg.member.hasPermission("MANAGE_MESSAGES")) {
-              if (msg.author.id !== ayarlar.yapimci) {
-                const mesajlariyonet = new Discord.RichEmbed()
-              .setColor(0xFF0000)
-              .setTimestamp()
-              .setAuthor(msg.author.username, msg.author.avatarURL)
-              .addField(':warning: Uyarı :warning:', 'Bu komutu kulllanmak için `Mesajları Yönet` iznine sahip olmalısın.')
-              return msg.author.sendEmbed(mesajlariyonet);
-          }}
-          msg.channel.bulkDelete(100);
-          msg.channel.bulkDelete(100);
-          msg.channel.bulkDelete(100);
-          msg.channel.bulkDelete(100);
-          msg.channel.bulkDelete(100);
-          msg.channel.bulkDelete(100);
-          msg.channel.bulkDelete(100);
-          msg.channel.bulkDelete(100);
-          msg.channel.bulkDelete(100);
-          msg.channel.bulkDelete(100); //1000 mesaj gg
-          const sohbetsilindi = new Discord.RichEmbed()
-        .setColor(0x00AE86)
-        .setTimestamp()
-        .addField('Eylem:', 'Sohbet silme')
-        .addField('Yetkili:', msg.author.username)
-        .addField('Sonuç:', `Başarılı`)
-        return msg.channel.sendEmbed(sohbetsilindi);
-          console.log("Sohbet " + msg.member + " tarafından silindi!");
-    }}};
+      if (msg.channel.type === 'dm') {
+        const ozelmesajuyari = new Discord.RichEmbed()
+      .setColor(0xFF0000)
+      .setTimestamp()
+      .setAuthor(msg.author.username, msg.author.avatarURL)
+      .addField(':warning: Uyarı :warning:', 'Bu komutu özel mesajlarda kullanamazsın.')
+      msg.author.sendEmbed(ozelmesajuyari); }
+        if (msg.channel.type !== 'dm') {
+          if (!msg.member.hasPermission("MANAGE_MESSAGES")) {
+            if (msg.author.id !== ayarlar.yapimci) {
+              const mesajlariyonet = new Discord.RichEmbed()
+            .setColor(0xFF0000)
+            .setTimestamp()
+            .setAuthor(msg.author.username, msg.author.avatarURL)
+            .addField(':warning: Uyarı :warning:', 'Bu komutu kulllanmak için `Mesajları Yönet` iznine sahip olmalısın.')
+            return msg.author.sendEmbed(mesajlariyonet);
+        }}
+        msg.channel.bulkDelete(100);
+        msg.channel.bulkDelete(100);
+        msg.channel.bulkDelete(100);
+        msg.channel.bulkDelete(100);
+        msg.channel.bulkDelete(100);
+        msg.channel.bulkDelete(100);
+        msg.channel.bulkDelete(100);
+        msg.channel.bulkDelete(100);
+        msg.channel.bulkDelete(100);
+        msg.channel.bulkDelete(100); //1000 mesaj gg
+        const sohbetsilindi = new Discord.RichEmbed()
+      .setColor(0x00AE86)
+      .setTimestamp()
+      .addField('Eylem:', 'Sohbet silme')
+      .addField('Yetkili:', msg.author.username)
+      .addField('Sonuç:', `Başarılı`)
+      return msg.channel.sendEmbed(sohbetsilindi);
+        console.log("Sohbet " + msg.member + " tarafından silindi!");
+  }}});
+  
+  client.on('message', msg => {
+    if (msg.content.toLowerCase() === prefix + 'reload') {
+      if (msg.author.id !== ayarlar.yapimci) {
+        const blnmyn = new Discord.RichEmbed()
+      .setColor(0xFF0000)
+      .setTimestamp()
+      .setAuthor(msg.author.username, msg.author.avatarURL)
+      .addField(':warning: Uyarı :warning:', 'Bu komutu kullanabilmek için `Bot Sahibi` yetkisine sahip olmalısın.')
+      return msg.channel.sendEmbed(blnmyn);
+      }
+      process.exit(1).catch(console.error);
+    }
+  });
+  
 
 // Bu olmak zorunda
 bot.login(process.env.BOT_TOKEN);
